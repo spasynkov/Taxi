c7e19cc2fae1bdbb3eb3af9632dfb5be0bb082da
Stas Pasynkov <s.pasynkov@gmail.com>
2024-01-31 14:15:21 +0200

    Fixed arrangement

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,14 +1,10 @@
 INSERT INTO refill (date, liters, price) VALUES ({ts '2023-11-24 00:00:00.00'}, 20, 1099.80);
-INSERT INTO refill (date, liters, price, discount)
-VALUES ({ts '2023-11-26 00:00:00.00'}, 15, 824.85, 80.4);
+INSERT INTO refill (date, liters, price, discount) VALUES ({ts '2023-11-26 00:00:00.00'}, 15, 824.85, 80.4);
 
 INSERT INTO ride (date, distance, type) VALUES ({ts '2023-11-25 00:00:00.00'}, 69, 1);
 INSERT INTO ride (date, distance, type) VALUES ({ts '2023-11-26 00:00:00.00'}, 79, 1);
 INSERT INTO ride (date, distance, type) VALUES ({ts '2023-11-27 00:00:00.00'}, 45, 1);
 
-INSERT INTO income (date, sum, type)
-VALUES ({ts '2023-11-25 00:00:00.00'}, 710.78, 0);
-INSERT INTO income (date, sum, type)
-VALUES ({ts '2023-11-26 00:00:00.00'}, 925.97, 0);
-INSERT INTO income (date, sum, type)
-VALUES ({ts '2023-11-27 00:00:00.00'}, 564.05, 0);
+INSERT INTO income (date, sum, type) VALUES ({ts '2023-11-25 00:00:00.00'}, 710.78, 0);
+INSERT INTO income (date, sum, type) VALUES ({ts '2023-11-26 00:00:00.00'}, 925.97, 0);
+INSERT INTO income (date, sum, type) VALUES ({ts '2023-11-27 00:00:00.00'}, 564.05, 0);
